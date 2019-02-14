312d4d6c501453448e20cb539e1414a7b1a203b4
Raul Gonzalez <raulg42@gmail.com>
2019-02-14 14:10:16 +0100

    404 styling
    Style for NotFound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <div>404 not found</div>;
+  document.title = "404 - Not Found";
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Document not found</h2>
+      <p><a href="/">Return to homepage</a></p>
+    </div>
+  );
 }
