7bb7ed166d4116d1502fe4bcf9ee67008c5e289c
Raul Gonzalez <raulg42@gmail.com>
2019-02-18 18:09:38 +0100

    Placeholder repository name
    your-repo-name

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -1,6 +1,6 @@
 export default {
 
-  apiEndpoint: 'https://sample-site.prismic.io/api/v2',
+  apiEndpoint: 'https://your-repo-name.prismic.io/api/v2',
 
   // -- Access token if the Prismic repository is not public
   // accessToken: 'xxxxxx',
