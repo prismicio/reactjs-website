7134841b05122fba65649ab4e3df43f016693a9d
Levi Gable <levimykel@gmail.com>
2019-11-18 18:03:51 +0100

    Refactor the NotFound component for readability

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => {
-  return (
-    <div className='not-found'>
-      <h1>404</h1>
-      <h2>Document not found</h2>
-      <p><Link to='/'>Return to homepage</Link></p>
-    </div>
-  )
-}
+/**
+ * Page not found (404) component
+ */
+const NotFound = () => (
+  <div className="not-found">
+    <h1>404</h1>
+    <h2>Document not found</h2>
+    <p><Link to="/">Return to homepage</Link></p>
+  </div>
+);
 
-export default NotFound
+export default NotFound;
