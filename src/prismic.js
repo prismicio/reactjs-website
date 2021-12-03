29ed52158f45350f654f3bc6271cc8fd51c170d0
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-12-02 16:09:41 -1000

    fix: restore placeholder repository name

--- src/prismic.js
+++ src/prismic.js
@@ -1,7 +1,7 @@
 import * as prismic from "@prismicio/client";
 
 // Fill in your repository name
-export const repositoryName = "react-website-angeloashmore";
+export const repositoryName = "your-repo-name";
 const endpoint = prismic.getEndpoint(repositoryName);
 
 export const client = prismic.createClient(endpoint, {
